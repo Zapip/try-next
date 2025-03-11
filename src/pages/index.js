a1a636e99657e58aaa793f709fd6fc1183cdad9f
Zapip <zafif3165@gmail.com>
2025-03-11 23:40:02 +0700

    feat(): added responsive UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 function SplitBill() {
   const [items, setItems] = useState([{ name: "", price: "", payer: "" }]);
   const [discount, setDiscount] = useState(null);
-  const [discountType, setDiscountType] = useState("price"); // New state for discount type
+  const [discountType, setDiscountType] = useState(null); // New state for discount type
   const [shipping, setShipping] = useState(null);
 
   const addItem = () => {
@@ -35,114 +35,116 @@ function SplitBill() {
     }).format(value);
   };
 
+  const getRandomColor = () => {
+    const colors = [
+      "bg-red-500",
+      "bg-green-500",
+      "bg-blue-500",
+      "bg-yellow-500",
+      "bg-purple-500",
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
   return (
-    <article className="m-12 flex gap-4">
-      <section className="flex flex-col gap-4">
+    <article className="flex flex-col sm:flex-row m-4 sm:m-12 gap-4">
+      <section className="flex flex-col w-full gap-4">
         <h1 className="text-xl font-bold text-center bg-gray-700 p-4 rounded-xl">
           Input Bill
         </h1>
-        <section className="bg-gray-700 p-6 rounded-xl w-fit text-black">
-          <table className="w-full mb-4">
-            <thead className="text-white">
-              <tr>
-                <th className=" bg-gray-800 p-2">Item</th>
-                <th className=" bg-gray-800 p-2">Harga</th>
-                <th className="bg-gray-800 p-2">Nama</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.map((item, index) => (
-                <tr key={index}>
-                  <td className="">
-                    <input
-                      type="text"
-                      placeholder="Item"
-                      className="w-full p-2"
-                      value={item.name}
-                      onChange={(e) =>
-                        updateItem(index, "name", e.target.value)
-                      }
-                    />
-                  </td>
-                  <td className="">
-                    <input
-                      type="number"
-                      placeholder="Harga"
-                      className="w-full p-2"
-                      value={item.price}
-                      onChange={(e) =>
-                        updateItem(index, "price", e.target.value)
-                      }
-                    />
-                  </td>
-                  <td className="">
-                    <input
-                      type="text"
-                      placeholder="Nama"
-                      className="w-full p-2"
-                      value={item.payer}
-                      onChange={(e) =>
-                        updateItem(index, "payer", e.target.value)
-                      }
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <section className="bg-gray-700 p-6 rounded-xl w-full text-black flex flex-col gap-4">
+          {items.map((item, index) => (
+            <div key={index} className="flex flex-col gap-2">
+              <section className="flex justify-between gap-2">
+                <input
+                  type="text"
+                  placeholder="Item"
+                  className="w-full p-2 rounded-lg"
+                  value={item.name}
+                  onChange={(e) => updateItem(index, "name", e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Nama"
+                  className="w-full p-2 rounded-lg"
+                  value={item.payer}
+                  onChange={(e) => updateItem(index, "payer", e.target.value)}
+                />
+              </section>
+              <input
+                type="number"
+                placeholder="Harga"
+                className="w-full p-2 rounded-lg"
+                value={item.price}
+                onChange={(e) => updateItem(index, "price", e.target.value)}
+              />
+            </div>
+          ))}
           <button
             onClick={addItem}
-            className="bg-orange-500 text-white px-4 py-2 rounded-md mt-2"
+            className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-md mt-2 font-bold"
           >
             Tambah Item
           </button>
         </section>
         <section className="bg-gray-700 p-6 rounded-xl w-full text-black">
-          <div className="mt-4 ">
-            <label className="block text-white">Diskon</label>
+          <div className="mt-4 w-full">
+            <label className="text-white">Diskon</label>
             <select
-              className="border p-2 w-full mb-2"
+              className="border p-2 w-full mb-2 rounded-lg"
               value={discountType}
               onChange={(e) => setDiscountType(e.target.value)}
             >
+              <option disabled selected>
+                Pilih Jenis Diskon
+              </option>
               <option value="price">Harga (Rp)</option>
               <option value="percent">Persen (%)</option>
             </select>
             <input
               type="number"
-              className="border p-2 w-full"
+              placeholder="0"
+              className="border p-2 w-full rounded-lg"
               value={discount}
               onChange={(e) => setDiscount(e.target.value)}
             />
           </div>
 
           <div className="mt-4">
-            <label className="block text-white">Ongkir (Rp)</label>
+            <label className="text-white">Ongkir (Rp)</label>
             <input
               type="number"
               placeholder="0"
-              className="border p-2 w-full"
+              className="border p-2 w-full rounded-lg"
               value={shipping}
               onChange={(e) => setShipping(e.target.value)}
             />
           </div>
         </section>
       </section>
-      <section className="size-full">
+      <section className="size-full flex flex-col gap-4">
         <h1 className="text-xl font-bold text-center bg-gray-700 p-4 rounded-xl">
           Preview Split Bill
         </h1>
-        <h2 className="text-lg font-bold mt-4">
+        <h2 className="text-lg font-bold bg-green-500 p-4 rounded-xl">
           Total: {formatCurrency(finalTotal)}
         </h2>
-        <h3 className="text-md font-semibold mt-2">Pembagian Biaya:</h3>
-        <ul className="list-disc pl-5">
-          {Object.entries(payments).map(([name, amount]) => (
-            <li key={name}>
-              {name}: {formatCurrency((amount / total) * finalTotal)}
-            </li>
-          ))}
-        </ul>
+        <section className="bg-gray-700 p-6 rounded-xl w-full flex flex-col gap-3">
+          <h3 className="text-md font-semibold mt-2">Pembagian Biaya:</h3>
+          <section className="flex flex-wrap gap-2 justify-center">
+            {Object.entries(payments).map(([name, amount]) => (
+              <section
+                className={`w-fit h-fit p-2 px-4 rounded-lg ${getRandomColor()}`}
+                key={name}
+              >
+                {`${name}: `}
+                <span className="font-bold">
+                  {formatCurrency((amount / total) * finalTotal)}
+                </span>
+              </section>
+            ))}
+          </section>
+        </section>
       </section>
     </article>
   );
